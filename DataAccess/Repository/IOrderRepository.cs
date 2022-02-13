15b9dd22e73c1b44b154d76b729e83650f5860ac
DzurahIvan <ivandzurah@gmail.com>
2022-02-13 18:25:10 +0200

    Add method GetOrdersByStatus

--- DataAccess/Repository/IOrderRepository.cs
+++ DataAccess/Repository/IOrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using DataAccess.Data;
 
 namespace DataAccess.Repository
@@ -6,6 +7,7 @@ namespace DataAccess.Repository
     public interface IOrderRepository
     {
         Task<Order> Get(int id);
-        Task<int> Upsert(Order order);
+        Task<List<Order>> GetOrdersByStatus(OrderStatus status);
+        Task<bool> Upsert(Order order);
     }
 }
