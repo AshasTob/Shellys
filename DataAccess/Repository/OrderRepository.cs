bfc07700b91b245cfc9289c860abe3a9d060191f
DzurahIvan <ivandzurah@gmail.com>
2022-02-13 18:26:36 +0200

    Implement GetOrdersByStatus

--- DataAccess/Repository/OrderRepository.cs
+++ DataAccess/Repository/OrderRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,35 +17,31 @@ namespace DataAccess.Repository
             _barDataBase = new BarDataBase();
         }
 
-        public async Task<Order> Get(int id)
+        public  Task<Order> Get(int id)
         {
-            Order order = await _barDataBase.Orders.FindAsync(id);
-            if(order == null)
-            {
-                throw new ArgumentException($"No order with {id} was found");
-            }
-            else
-            {
-                return order;
-            }
+            Order order = _barDataBase.Orders.Find(id);
+            return Task.FromResult(order);
         }
 
-        public async Task<int> Upsert(Order order)
+        public Task<List<Order>> GetOrdersByStatus(OrderStatus status)
         {
-            bool ord = _barDataBase.Orders.Contains(order);
-            int affrcted;
+            return Task<List<Order>>.FromResult(_barDataBase.Orders.Where(order => order.Status == status).ToList());
+        }
+
+        public Task<bool> Upsert(Order order)
+        {
+            bool ord = _barDataBase.Orders.Contains(order);          
             if (ord == false)
             {
                 _barDataBase.Orders.Add(order);
-                affrcted = await _barDataBase.SaveChangesAsync();
-                return (affrcted == 1) ? -201 : affrcted;
             }
             else
             {
                 _barDataBase.Orders.Update(order);
-                affrcted = await _barDataBase.SaveChangesAsync();
-                return (affrcted == 1) ? -204 : affrcted;
             }
+            int affected = _barDataBase.SaveChanges();
+            bool isUpsert = (affected == 1) ? true : false;
+            return Task.FromResult(isUpsert);
         }
     }
 }
