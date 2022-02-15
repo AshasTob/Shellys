3f018dcf9e06736dd0ece765f3208976b277acc8
DzurahIvan <ivandzurah@gmail.com>
2022-02-16 01:22:42 +0200

    Implement report feature

--- OrderTimeOutService/OrderTimeOut.cs
+++ OrderTimeOutService/OrderTimeOut.cs
@@ -1,22 +1,30 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using DataAccess.Data;
 using DataAccess.Repository;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using OrderTimeOutService.BlobService;
+using System.Globalization;
 
 namespace OrderTimeOutService
 {
     public  class OrderTimeOut
     {
         private readonly OrderRepository _orderRepository;
+        private readonly BlobStorage _blobStorage;
+        private readonly IConfigurationBuilder _configuration;
 
         public OrderTimeOut()
         {
             _orderRepository = new OrderRepository();
+            _configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            _blobStorage = new BlobStorage(_configuration);
         }
 
         [FunctionName("OrderTimeOut")]
@@ -24,11 +32,17 @@ namespace OrderTimeOutService
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             List<Order> initiatedOrders = await _orderRepository.GetOrdersByStatus(OrderStatus.Initiated);
+            double finalizedOrdersSum = 0;
             foreach (var order in initiatedOrders)
             {
                 order.Status = OrderStatus.Finalized;
+                finalizedOrdersSum += order.TotalPrice;
                 await _orderRepository.Upsert(order);
             }
+            string reportData = $"{initiatedOrders.Count},{finalizedOrdersSum.ToString(new CultureInfo("en-us",false))}";
+            DateTime dateTime = DateTime.Now;
+            string reportFileName = $"{dateTime.Day}_{dateTime.Month}_{dateTime.Year}_report.csv";
+            await _blobStorage.UploadContentBlobAsync(reportData, reportFileName);
         }
     }
 }
